5291f762d1afc63e5776db7310194ffbc214e798
MacBook <macbook@MacBooks-MacBook-Pro.local>
2024-07-22 21:55:15 -0400

    Simulador de Cafeteria Online

--- Js.js
+++ Js.js
@@ -1,7 +1,4 @@
 
-// const cafeOpciones = ["Espresso: $1.500", "Latte: $2.500", "Cappuccino: $2.000"];
-// const precios = [1500, 2500, 2000];
-// const promocion = "Latte + 2 medias lunas"; 
 function calcularPrecioCafe() {
     let cafe1 = "espresso";
     let cafe2 = "Capuchino";
@@ -41,44 +38,3 @@ function mostrarResultado(precio) {
 
 calcularPrecioCafe();
 
-
-
-// function mostrarOpciones() {
-//   let mensaje = "Elige tu café:\n";
-//   for (let i = 0; i < cafeOpciones.length; i++) { 
-//     mensaje += `${i + 1}. ${cafeOpciones[i]} - $${precios[i]}\n`;
-//   }
-//   mensaje += "4. Salir";
-//   return mensaje;
-// }
-
-// function calcularPrecio(opcion) {
-//   let precio = precios[opcion - 1];
-//   if (cafeOpciones[opcion - 1] === promocion) {
-//     precio = 3000; 
-//   }
-//   return precio;
-// }
-
-
-// function simuladorCafeteria() {
-//   let continuar = true;
-  
-//   while (continuar) {
-//     const eleccion = prompt(mostrarOpciones());
-    
-//     if (eleccion >= 1 && eleccion <= 3) {
-//       const precioFinal = calcularPrecio(eleccion);
-//       alert(`Has elegido un ${cafeOpciones[eleccion - 1]}. El precio es $${precioFinal.toFixed(0)}.`);
-//       alert("Tu café está listo y en camino con el delivery.");
-//     } else if (eleccion == 4) {
-//       continuar = false;
-//       alert("Gracias por visitar nuestra cafetería online.");
-//     } else {
-//       alert("Por favor, elige una opción válida.");
-//     }
-//   }
-// }
-
-
-// simuladorCafeteria();
